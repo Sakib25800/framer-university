7b88466fb75f2ec33e8c513b236eca2f5cdb2d41
Sakibul Islam <sakibulislam25800@gmail.com>
2025-06-05 15:27:12 +0100

    web: add favicon

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,34 +3,20 @@ import type { Metadata } from "next";
 import Providers from "./providers";
 
 export const metadata: Metadata = {
-  title: "Learn with Framer University",
-  description: "Learn everything there is to know about Framer.",
-  icons: [
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/images/icon-light.png",
-      media: "(prefers-color-scheme: light)"
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/images/icon-dark.png",
-      media: "(prefers-color-scheme: dark)"
-    }
-  ]
+    title: "Learn with Framer University",
+    description: "Learn everything there is to know about Framer.",
 };
 
 export default function RootLayout({
-  children
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="bg-black antialiased">
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className="bg-black antialiased">
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
 }
