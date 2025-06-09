1858894277f21685d9d292204b4d647eb0d1f50d
Sakibul Islam <sakibulislam25800@gmail.com>
2025-06-09 20:57:21 +0100

    chore: rename packages to package for commit type

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,22 @@
 export default {
-    rules: {
-        'type-enum': [
-            2,
-            'always',
-            ['admin', 'api', 'storybook', 'web', 'chore', 'docs', 'packages', 'ci', 'build'],
-        ],
-        'subject-empty': [2, 'never'],
-        'type-case': [2, 'always', 'lower-case'],
-        'type-empty': [2, 'never'],
-    },
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "admin",
+        "api",
+        "storybook",
+        "web",
+        "chore",
+        "docs",
+        "package",
+        "ci",
+        "build"
+      ]
+    ],
+    "subject-empty": [2, "never"],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"]
+  }
 };
