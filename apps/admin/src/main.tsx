1ab77678a7a4e3643b571100df4f96dc0e8c0310
Sakibul Islam <sakibulislam25800@gmail.com>
2025-06-10 15:04:50 +0100

    admin: remove redundant comment

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./app";
 import "./globals.css";
 
-// Test smart deployment - only admin should deploy
-
 const el = document.getElementById("root");
 if (el) {
   const root = createRoot(el);
