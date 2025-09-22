31bd70b5f34b80b400f2adb527a2a605d2619d2e
Sakibul Islam <sakibulislam25800@gmail.com>
2025-09-22 18:32:00 +0100

    Button refinments

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,12 +1,12 @@
 import { cva, type VariantProps } from "class-variance-authority"
-import { type HTMLMotionProps, motion } from "framer-motion"
+import { motion } from "framer-motion"
 import { twMerge } from "tailwind-merge"
 
 const button = cva(["inline-flex", "items-center", "justify-center", "text-center", "rounded-full", "cursor-pointer"], {
   variants: {
     intent: {
-      primary: ["bg-white", "!text-black"],
       outline: ["border", "border-primary-400"],
+      primary: ["bg-white", "!text-black"],
     },
     size: {
       sm: ["text-base", "font-semibold", "px-4", "py-[7px]"],
@@ -29,18 +29,20 @@ export interface ButtonProps extends VariantProps<typeof button> {
 }
 
 export function Button({ className, intent, size, children, ...props }: ButtonProps) {
-  const motionProps: HTMLMotionProps<"button"> = {
-    className: twMerge(button({ intent, size, className })),
-    whileHover: intent === "outline" ? { borderColor: "white" } : { filter: "brightness(0.8)" },
-    whileTap: intent === "outline" ? { scale: 0.95 } : { scale: 0.95 },
-    transition: {
-      duration: 0.3,
-      type: "spring",
-      bounce: 0.2,
-      delay: 0,
-    },
-    ...props,
-  }
-
-  return <motion.button {...motionProps}>{children}</motion.button>
+  return (
+    <motion.button
+      className={twMerge(button({ intent, size, className }))}
+      whileHover={intent === "outline" ? { borderColor: "white" } : { filter: "brightness(0.8)" }}
+      whileTap={intent === "outline" ? { scale: 0.95 } : { scale: 0.95 }}
+      transition={{
+        duration: 0.3,
+        type: "spring",
+        bounce: 0.2,
+        delay: 0,
+      }}
+      {...props}
+    >
+      {children}
+    </motion.button>
+  )
 }
