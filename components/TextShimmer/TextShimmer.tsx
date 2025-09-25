56734e763867ad4d933d5b60a036157de7803b28
Sakibul Islam <sakibulislam25800@gmail.com>
2025-09-25 15:47:16 +0100

    Refine text shimmer

--- components/TextShimmer/TextShimmer.tsx
+++ components/TextShimmer/TextShimmer.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import * as React from "react"
+import { twMerge } from "tailwind-merge"
+import styles from "./TextShimmer.module.css"
 
 export type TextShimmerDirection = "left" | "right"
 
@@ -13,14 +15,10 @@ export interface TextShimmerProps {
   userSelect?: boolean
   rotation?: number
   delay?: number
-  tag?: keyof JSX.IntrinsicElements
+  tag?: keyof React.JSX.IntrinsicElements
   direction?: TextShimmerDirection
   style?: React.CSSProperties
   className?: string
-}
-
-function getUniqueClassName() {
-  return "text-shimmer-" + Math.random().toString(36).substr(2, 9)
 }
 
 export function TextShimmer({
@@ -37,105 +35,32 @@ export function TextShimmer({
   style,
   className,
 }: TextShimmerProps) {
-  const classNameRef = React.useRef<string>(getUniqueClassName())
   const Tag = tag as any
-  const backgroundSize = `200% 200%`
-  const speedFormatted = 11 - speed
-  const gradientFirst = 50 - (size * (50 - 20)) / 100
-  const gradientLast = 50 + (size * (80 - 50)) / 100
-  const animationDuration = speedFormatted + delay
 
+  const speedFormatted = 11 - speed
+  const gradFirst = 50 - (size * (50 - 20)) / 100
+  const gradLast = 50 + (size * (80 - 50)) / 100
+  const durationSec = `${speedFormatted + delay}s`
   const startPos = direction === "left" ? "-100%" : "200%"
   const endPos = direction === "left" ? "200%" : "-100%"
-  const keyframes = `
-        @keyframes shimmer-${classNameRef.current} {
-            0%, ${(delay / animationDuration) * 100}% {
-                background-position: ${startPos};
-            }
-            100% {
-                background-position: ${endPos};
-            }
-        }
-    `
-
-  React.useEffect(() => {
-    const styleId = `shimmer-style-${classNameRef.current}`
-    let styleEl = document.getElementById(styleId) as HTMLStyleElement | null
-    if (!styleEl) {
-      styleEl = document.createElement("style")
-      styleEl.id = styleId
-      document.head.appendChild(styleEl)
-    }
-    styleEl.innerHTML = `
-            ${keyframes}
-            .${classNameRef.current} {
-                background: linear-gradient(
-                    ${rotation}deg,
-                    transparent 0%,
-                    transparent ${gradientFirst}%,
-                    ${shimmer} 50%,
-                    transparent ${gradientLast}%,
-                    transparent 100%
-                );
-                -webkit-background-size: ${backgroundSize};
-                -moz-background-size: ${backgroundSize};
-                background-size: ${backgroundSize};
-                -webkit-background-clip: text;
-                -moz-background-clip: text;
-                background-clip: text;
-                background-repeat: no-repeat;
-                animation: shimmer-${classNameRef.current} ${animationDuration}s linear infinite;
-            }
-        `
-    return () => {
-      if (styleEl && styleEl.parentNode) {
-        styleEl.parentNode.removeChild(styleEl)
-      }
-    }
-  }, [
-    shimmer,
-    rotation,
-    gradientFirst,
-    gradientLast,
-    backgroundSize,
-    animationDuration,
-    delay,
-    direction,
-    keyframes,
-  ])
 
   return (
-    <div
-      style={{
-        userSelect: userSelect ? "auto" : "none",
-        position: "relative",
-      }}
-      className={className}
-    >
-      <Tag
-        style={{
-          color,
-          marginBlockStart: "0px",
-          marginBlockEnd: "0px",
-          ...style,
-        }}
-      >
+    <div className={twMerge("relative select-none", userSelect && "select-auto", className)}>
+      <Tag className={twMerge("m-0", !style?.fontSize && "") as string} style={{ color, ...style }}>
         {content}
       </Tag>
       <Tag
         aria-hidden="true"
-        className={classNameRef.current}
+        className={twMerge(styles.layer)}
         style={{
-          color: "transparent",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          zIndex: 1,
-          marginBlockStart: "0px",
-          marginBlockEnd: "0px",
-          pointerEvents: "none",
+          ["--ts-rotation" as any]: `${rotation}deg`,
+          ["--ts-grad-first" as any]: `${gradFirst}%`,
+          ["--ts-grad-last" as any]: `${gradLast}%`,
+          ["--ts-shimmer" as any]: shimmer,
+          ["--ts-duration" as any]: durationSec,
+          ["--ts-delay" as any]: `${delay}s`,
+          ["--ts-start" as any]: startPos,
+          ["--ts-end" as any]: endPos,
           ...style,
         }}
       >
