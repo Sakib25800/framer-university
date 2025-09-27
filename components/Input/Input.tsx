7b9f1886900bfc2b2e30c1a0694caf2ad08098f7
Sakibul Islam <sakibulislam25800@gmail.com>
2025-09-27 11:15:14 +0100

    Refactor input

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -17,11 +17,7 @@ const input = cva([
   "focus:shadow-[inset_0_1px_0_0_rgba(255,255,255,0.05),0_2px_4px_0_rgba(0,0,0,0.1),0_0_0_2px_#161717,0_0_0_3px_#00bbff]",
 ])
 
-export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "className"> {
-  className?: string
-}
-
-export function Input({ className, ...props }: InputProps) {
+export function Input({ className, ...props }: React.ComponentProps<"input">) {
   return <input className={cn(input({ className }))} {...props} />
 }
 
