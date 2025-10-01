2a9fcddddf6b806f64d6f57ca372c110e0fdd904
Sakibul Islam <sakibulislam25800@gmail.com>
2025-10-01 17:04:52 +0100

    Refine quote props

--- components/Quote/Quote.tsx
+++ components/Quote/Quote.tsx
@@ -1,22 +1,20 @@
-import * as React from "react"
 import { cva } from "class-variance-authority"
+import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export interface QuoteProps extends React.ComponentProps<"div"> {}
-
-const quote = cva(["flex", "gap-5"]) 
+const quote = cva(["flex", "gap-5"])
 const quoteContent = cva([
   "text-primary-950",
   "body-l",
   "font-medium",
   "[&_strong]:text-white",
   "[&_strong]:font-medium",
-]) 
+])
 
-export function Quote({ children, className, ...props }: QuoteProps) {
+export function Quote({ children, className, ...props }: React.ComponentProps<"div">) {
   return (
     <div className={cn(quote(), className)} {...props}>
-      <div className="w-[2px] bg-accent flex-shrink-0" />
+      <div className="bg-accent w-[2px] flex-shrink-0" />
       <div className={quoteContent()}>{children}</div>
     </div>
   )
